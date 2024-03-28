83b14df1e723ccddc04616f4d58ff35f0dd7cd45
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-03-28 20:03:55 +0400

    I think I am into smthng

--- src/net/not_a_cat/ship_systems/InvisibilityShipsystem.java
+++ src/net/not_a_cat/ship_systems/InvisibilityShipsystem.java
@@ -8,23 +8,27 @@ import com.fs.starfarer.api.plugins.ShipSystemStatsScript;
 import java.awt.Color;
 
 public class InvisibilityShipsystem extends BaseShipSystemScript {
-    Long systemActivationMillis = 0L;
+    ShipSystemStatsScript.State previousState = State.IDLE;
+    ShipVariantAPI invincibleVariant = Global.getSettings().createEmptyVariant("not_a_cat_lobster_invincible", Global.getSettings().getHullSpec("not_a_cat_lobster_invincible"));
+    ShipAPI invincibleShip = Global.getCombatEngine().createFXDrone(invincibleVariant);
+
+    ShipAPI originalHost;
 
     @Override
     public String getInfoText(ShipSystemAPI system, ShipAPI ship) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getInfoText(%s, %s)", system.getId(), ship.getId()));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getInfoText(%s, %s)", system.getId(), ship.getId()));
         return super.getInfoText(system, ship);
     }
 
     @Override
     public boolean isUsable(ShipSystemAPI system, ShipAPI ship) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::isUsable(%s, %s)", system.getId(), ship.getId()));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::isUsable(%s, %s)", system.getId(), ship.getId()));
         return super.isUsable(system, ship);
     }
 
     @Override
     public float getActiveOverride(ShipAPI ship) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getActiveOverride(%s)", ship.getId()));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getActiveOverride(%s)", ship.getId()));
         return super.getActiveOverride(ship);
     }
 
@@ -37,120 +41,119 @@ public class InvisibilityShipsystem extends BaseShipSystemScript {
 
     @Override
     public float getOutOverride(ShipAPI ship) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getOutOverride(%s)", ship.getId()));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getOutOverride(%s)", ship.getId()));
         return super.getOutOverride(ship);
     }
 
     @Override
     public float getRegenOverride(ShipAPI ship) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getRegenOverride(%s)", ship.getId()));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getRegenOverride(%s)", ship.getId()));
         return super.getRegenOverride(ship);
     }
 
     @Override
     public int getUsesOverride(ShipAPI ship) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getUsesOverride(%s)", ship.getId()));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getUsesOverride(%s)", ship.getId()));
         return super.getUsesOverride(ship);
     }
 
     @Override
     public String getDisplayNameOverride(State state, float effectLevel) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getDisplayNameOverride(%s, %f)", state, effectLevel));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getDisplayNameOverride(%s, %f)", state, effectLevel));
         return super.getDisplayNameOverride(state, effectLevel);
     }
 
     @Override
     public void apply(MutableShipStatsAPI stats, String id, ShipSystemStatsScript.State state, float effectLevel) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::apply(%s, %s, %s, %f)", stats, id, state, effectLevel));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::apply(%s, %s, %f)", id, state, effectLevel));
         if (!(stats.getEntity() instanceof ShipAPI)) {
             return;
         }
 
-        Long currentTime = System.currentTimeMillis();
         ShipAPI hostShip = (ShipAPI) stats.getEntity();
-        Global.getLogger(InvisibilityShipsystem.class).info("Invisibility: " + state);
         switch (state) {
             case IDLE:
                 break;
-            case ACTIVE:
-                for (WeaponAPI weapon : hostShip.getAllWeapons()) {
-                    if (weapon.isFiring()) {
-                        hostShip.getSystem().deactivate();
-                    }
+            case IN:
+                if(previousState == State.IDLE){
+                    // Just activated
+                    phaseIn(hostShip);
                 }
                 break;
-            case IN:
-                if(systemActivationMillis == 0L){
-                    systemActivationMillis = phaseIn(hostShip);
-                } else if (300L < currentTime - systemActivationMillis){
+            case ACTIVE:
+                if(previousState == State.IN){
                     becomeInvisible(hostShip);
                     phaseOut(hostShip);
-                } else if (3000L < currentTime - systemActivationMillis){
-                    becomeVisible(hostShip);
                 }
                 break;
             case OUT:
-                becomeVisible(hostShip);
-                systemActivationMillis = 0L;
+                if(previousState == State.ACTIVE){
+                    becomeVisible(hostShip);
+                }
                 break;
             case COOLDOWN:
-                systemActivationMillis = 0L;
                 break;
         }
+        previousState = state;
     }
 
     @Override
     public void unapply(MutableShipStatsAPI stats, String id) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::unapply(%s, %s, %s, %f)", stats, id));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::unapply(%s)", id));
     }
 
-    Long phaseIn(ShipAPI hostShip) {
+    void phaseIn(ShipAPI hostShip) {
         Global.getLogger(InvisibilityShipsystem.class).info(String.format("::phaseIn(%s)", hostShip.getId()));
         ShipSystemAPI cloak = hostShip.getPhaseCloak();
         if (cloak == null || !cloak.canBeActivated()) {
-            return 0L;
+            return;
         }
         cloak.forceState(ShipSystemAPI.SystemState.ACTIVE, 0f);
         hostShip.setHoldFireOneFrame(true);
-        return System.currentTimeMillis();
     }
 
-    Long becomeInvisible(ShipAPI hostShip) {
+    void becomeInvisible(ShipAPI hostShip) {
         Global.getLogger(InvisibilityShipsystem.class).info(String.format("::becomeInvisible(%s)", hostShip.getId()));
-//        hostShip.getMutableStats().getHullDamageTakenMult().modifyMult("invisibility", 0f);
-//        hostShip.getMutableStats().getArmorDamageTakenMult().modifyMult("invisibility", 0f);
-//        hostShip.getMutableStats().getEngineDamageTakenMult().modifyMult("invisibility", 0f);
 
-        Global.getCombatEngine().removeEntity(hostShip);
-
-        Global.getCombatEngine().getPlayerShip();
-
-//        Global.getCombatEngine().addEntity();
-
-        return System.currentTimeMillis();
+        invincibleShip.getLocation().set(hostShip.getLocation());
+        invincibleShip.getVelocity().set(hostShip.getVelocity());
+        invincibleShip.setAngularVelocity(hostShip.getAngularVelocity());
+        invincibleShip.setFacing(hostShip.getFacing());
+        originalHost = hostShip;
+        Global.getCombatEngine().addEntity(invincibleShip);
+        if(hostShip.getId() == Global.getCombatEngine().getPlayerShip().getId()) {
+            Global.getCombatEngine().setPlayerShipExternal(invincibleShip);
+        }
+        hostShip.getLocation().set(10000f, 10000f); // Far far away
     }
-    Long phaseOut(ShipAPI hostShip) {
+    void phaseOut(ShipAPI hostShip) {
         Global.getLogger(InvisibilityShipsystem.class).info(String.format("::phaseOut(%s)", hostShip.getId()));
         ShipSystemAPI cloak = hostShip.getPhaseCloak();
         if (cloak == null || !cloak.canBeActivated()) {
-            return 0L;
+            return;
         }
 
         cloak.forceState(ShipSystemAPI.SystemState.IDLE, 0f);
-        return 0L;
     }
 
     void becomeVisible(ShipAPI hostShip){
         Global.getLogger(InvisibilityShipsystem.class).info(String.format("::becomeVisible(%s)", hostShip.getId()));
 
-//        hostShip.getMutableStats().getHullDamageTakenMult().modifyMult("invisibility", 1f);
-//        hostShip.getMutableStats().getArmorDamageTakenMult().modifyMult("invisibility", 1f);
-//        hostShip.getMutableStats().getEngineDamageTakenMult().modifyMult("invisibility", 1f);
+        hostShip.getLocation().set(invincibleShip.getLocation());
+        hostShip.getVelocity().set(invincibleShip.getVelocity());
+        hostShip.setAngularVelocity(invincibleShip.getAngularVelocity());
+        hostShip.setFacing(invincibleShip.getFacing());
+
+        if(invincibleShip.getId() == Global.getCombatEngine().getPlayerShip().getId()) {
+            Global.getCombatEngine().setPlayerShipExternal(hostShip);
+        }
+
+        Global.getCombatEngine().removeEntity(invincibleShip);
     }
 
     @Override
     public StatusData getStatusData(int index, ShipSystemStatsScript.State state, float effectLevel) {
-        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getStatusData(%d, %s, %f)", index, state, effectLevel));
+//        Global.getLogger(InvisibilityShipsystem.class).info(String.format("::getStatusData(%d, %s, %f)", index, state, effectLevel));
         return super.getStatusData(index, state, effectLevel);
     }
 }
